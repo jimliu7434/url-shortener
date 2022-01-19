4de0503d0f8e1c75155629e926f7c01e961ee5ff
jim.liu <jim.liu@mitake.com.tw>
2022-01-19 23:10:03 +0800

    unit test

--- _test/main_test.go
+++ _test/main_test.go
@@ -35,20 +35,35 @@ func init() {
 func Test_createOK(t *testing.T) {
 	router := router.SetupRouter(isDebugMode)
 
-	// mock redis data
-	db, _ := redismock.NewClientMock()
-	model.NewURL(db)
-
-	w := httptest.NewRecorder()
+	// post body string
 	body := handler.ReqCreate{
 		URL:       targetURL,
 		ExpiredAt: time.Now().Add(5 * time.Second),
 	}
 	j, _ := json.Marshal(body)
+
+	// mock redis data
+	db, mock := redismock.NewClientMock()
+	mock.Regexp().ExpectSetNX(`.*`, `.*`, 0).SetVal(true)
+	mock.Regexp().ExpectExpireAt(`.*`, body.ExpiredAt).SetVal(true)
+	model.NewURL(db)
+
+	// call api
+	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("POST", "/api/v1/urls", strings.NewReader(string(j)))
 	req.Header.Set("Content-Type", "application/json")
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
+
+	var respBody handler.RespCreate
+	err := json.Unmarshal(w.Body.Bytes(), &respBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Fatal(err)
+	}
 }
 
 func Test_redirectOK(t *testing.T) {
@@ -59,12 +74,18 @@ func Test_redirectOK(t *testing.T) {
 	// mock redis data
 	db, mock := redismock.NewClientMock()
 	model.NewURL(db)
+	model.URL.SetNX(key, targetURL)
 	mock.ExpectGet(fmt.Sprintf("URL::%s", key)).SetVal(targetURL)
 
+	// call api
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", fmt.Sprintf("/%s", key), nil)
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusFound, w.Code)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Fatal(err)
+	}
 }
 
 func Test_redirectNotFound(t *testing.T) {
@@ -77,10 +98,15 @@ func Test_redirectNotFound(t *testing.T) {
 	model.NewURL(db)
 	mock.ExpectGet(fmt.Sprintf("URL::%s", key)).RedisNil()
 
+	// call api
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", fmt.Sprintf("/%s", key), nil)
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusNotFound, w.Code)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Fatal(err)
+	}
 }
 
 func Test_redirectExpired(t *testing.T) {
@@ -91,13 +117,18 @@ func Test_redirectExpired(t *testing.T) {
 	// mock redis data
 	db, mock := redismock.NewClientMock()
 	model.NewURL(db)
-	mock.ExpectGet(fmt.Sprintf("URL::%s", key)).SetVal(targetURL)
-	mock.ExpectExpire(fmt.Sprintf("URL::%s", key), 3*time.Second)
+	model.URL.SetNXWithExpireTime(key, targetURL, time.Now().Add(3*time.Second))
+	mock.ExpectGet(fmt.Sprintf("URL::%s", key)).RedisNil()
 
+	// call api
 	time.Sleep(5 * time.Second)
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", fmt.Sprintf("/%s", key), nil)
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusNotFound, w.Code)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Fatal(err)
+	}
 }
